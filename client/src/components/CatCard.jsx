6019479d3ed4b9953a77ee0929debaf01584b736
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-08 23:13:54 +0700

    fix: change home page design

--- client/src/components/CatCard.jsx
+++ client/src/components/CatCard.jsx
@@ -5,11 +5,11 @@ export default function CatCard({ item }) {
     return (
         <>
             <div className="border shrink-0 w-64 h-64">
-                <Link to={`/items/${item.id}`} className='tooltip tooltip-right' data-tip={item.name}>
+                <Link to={`/items/${item.id}`} className='tooltip tooltip-right w-full h-full' data-tip={item.name}>
                     <img
                         src={item.imageUrl ? item.imageUrl : defaultPict}
                         alt={`${item.name}-pict`}
-                        className="w-64 h-64 object-cover"
+                        className="w-full h-full object-cover"
                     />
                 </Link>
             </div>
